325baa722760290a5dadfe246e25d891d57e9c98
aliadamovich <alesya.argentina@gmail.com>
2023-11-27 11:10:28 -0300

    header and cover

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+//*МЕНЮ БУРГЕР=================
+// const iconMenu = document.querySelector('.menu__icon');
+// if (iconMenu) {
+// 	const menuBody = document.querySelector('.menu__body');
+// 	iconMenu.addEventListener("click", function (e) {
+// 		document.body.classList.toggle('_lock');
+// 		iconMenu.classList.toggle('_active');
+// 		menuBody.classList.toggle('_active');
+// 	});
+// }
+//*============================
+
+
+//*СЛАЙДЕР ===============
+
+//*============================
+// document.addEventListener('mousemove', parallax);
+// document.addEventListener('mouseout', resetParallax);
+// const parallaxItems = document.querySelectorAll('.layer');
+// function parallax(e) {
+// 	if (e.target.closest('.cover')) {
+// 		parallaxItems.forEach((item) => {
+// 			const speed = item.getAttribute('data-speed')
+// 			//ширина окна браузера - координаты события * на скорость
+// 			const x = (window.innerWidth - e.pageX * speed) / 100
+// 			const y = (window.innerHeight - e.pageY * speed) / 100
+// 			item.style.transform = `translateX(${x}px) translateY(${y}px)`
+// 		})
+// 	}
+// }
+
+// function resetParallax() {
+// 	parallaxItems.forEach((item) => {
+// 		item.style.transform = 'translateX(0) translateY(0)'
+// 	})
+// }
